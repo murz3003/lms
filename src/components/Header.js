8da64ab3babc679d427a6d97beb08e061c66178d
Dav Hill <originaldavz@gmail.com>
2017-04-06 23:24:52 +0100

    Use connect instead of <Route> for Header component
    Seemed silly to render a route when all it needed was a location pathname

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Route, Link } from 'react-router-dom';
 import classNames from 'classnames';
 
 import '../css/Header.css';
 import logo from '../images/logo.svg';
 
-const Header = () => {
-    return (
-        <Route children={({ location }) => {
-            const className = classNames({
-                header: true,
-                'header-small': location.pathname !== '/'
-            });
+const Header = (props) => {
+    const { pathname } = props.router.location;
+    const className = classNames({
+        header: true,
+        'header-small': pathname !== '/'
+    });
 
-            return (
-                <div className={className}>
-                    <Link to="/">
-                        <img src={logo} className="header-logo" alt="logo" />
-                        <h2 className="header-title">{location.pathname === '/' ? 'Welcome to LMS' : 'Last Man Standing'}</h2>
-                    </Link>
-                </div>
-            );
-        }} />
+    return (
+        <div className={className}>
+            <Link to="/">
+                <img src={logo} className="header-logo" alt="logo" />
+                <h2 className="header-title">{pathname === '/' ? 'Welcome to LMS' : 'Last Man Standing'}</h2>
+            </Link>
+        </div>
     );
 }
 
-export default Header;
+function mapStateToProps (state) {
+    return {
+        router: state.router
+    };
+}
+
+export default connect(mapStateToProps)(Header);
